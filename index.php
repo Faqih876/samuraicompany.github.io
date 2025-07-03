c298b1a0d14b9ce81c82026e06f5a76aac601279
Mursyid 876 <mursyidallfaqih@gmail.com>
2025-07-03 11:54:34 +0700

    menambahkan ubah si php jadi ke html

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
-header("location: views/home.php?page=home");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coba Git Remote</title> 
+</head>
+<body> 
+    <h1>HelloWorld!<h1>
+    <p>Hallo<p>
+<ul>
+    <li>satu<li>
+    <li>dua<li>
+<ul>    
+</body>   
+<html>     
+
